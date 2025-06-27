64ced8f77c545596fa76c13079aaacf4108f2f57
Vladislav <kalachinskii_vl@mail.ru>
2025-06-28 00:26:05 +0300

    убраны лишние подключения с апп

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,5 @@
-import { About } from "@/shared/ui/About/About";
-import { Catalog } from "@/shared/ui/Catalog/Catalog";
-import { Footer } from "@/shared/ui/Footer/Footer";
-// import Header from "@/shared/ui/Header/Header";
-import Map from "@/shared/ui/LeafletMap/LeafletMap";
-import { MarqueeLeftRight } from "@/shared/ui/MarqueeLeftRight/MarqueeLeftRight";
-import { MarqueeRightLeft } from "@/shared/ui/MarqueeRightLeft/MarqueeRightLeft";
-import Reviews from "@/shared/ui/Reviews/Reviews";
-import { TopCarousel } from "@/shared/ui/TopCarousel/TopCarousel";
-import { Trend } from "@/shared/ui/Trend/Trend";
-import styles from "./App.module.css";
-import Header from "@/shared/ui/Header/Header";
 import { AppRouter } from "./AppRouter";
+import styles from "./App.module.css";
 
 export function App() {
     return (
